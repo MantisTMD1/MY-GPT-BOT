d89a3012864252003cf4d5fbfc9427d05add5dad
Evan St Martin <mantisTMD1@gmail.com>
2023-03-30 10:01:45 -0500

    chatgpt project

---
+++ terminal-bot.js
@@ -0,0 +1,57 @@
+const { Configuration, OpenAIApi } = require("openai");
+// command line prompt package
+const readlineSync = require("readline-sync");
+require("dotenv").config();
+
+(async () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  //   empty object that fills with conversation
+  const history = [];
+
+  while (true) {
+    const user_input = readlineSync.question(
+      "Ask me anything! Your input here: "
+    );
+
+    const messages = [];
+    for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
+    }
+
+    messages.push({ role: "user", content: user_input });
+
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      const completion_text = completion.data.choices[0].message.content;
+      console.log(completion_text);
+
+      history.push([user_input, completion_text]);
+
+      const user_input_again = readlineSync.question(
+        "\nWould you like to continue the conversation? (y/n)"
+      );
+      if (user_input_again.toUpperCase() === "n") {
+        return;
+      } else if (user_input_again.toUpperCase() === "y") {
+        console.log("Invalid input. Please enter 'y' or 'n'.");
+        return;
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
+  }
+})();
